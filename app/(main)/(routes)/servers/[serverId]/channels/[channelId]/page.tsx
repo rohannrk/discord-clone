08d191889b3c5efd6ccd3fe061f46fe1e22356d0
rohannrk <rohanrk39357@gmail.com>
2023-09-25 02:10:05 +0530

    Socket

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,9 +4,9 @@ import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
-// import { ChatInput } from "@/components/chat/chat-input";
-// import { ChatMessages } from "@/components/chat/chat-messages";
-// import { MediaRoom } from "@/components/media-room";
+import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
+import { MediaRoom } from "@/components/media-room";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -49,7 +49,7 @@ const ChannelIdPage = async ({
         serverId={channel.serverId}
         type="channel"
       />
-      {/* {channel.type === ChannelType.TEXT && (
+      {channel.type === ChannelType.TEXT && (
         <>
           <ChatMessages
             member={member}
@@ -89,7 +89,7 @@ const ChannelIdPage = async ({
           video={true}
           audio={true}
         />
-      )} */}
+      )}
     </div>
    );
 }
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,7 +1,8 @@
 import { Hash } from "lucide-react";
 
 import { UserAvatar } from "@/components/user-avatar";
-import { MobileToggle } from "../mobile-toggle";
+import { MobileToggle } from "@/components/mobile-toggle";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -31,6 +32,9 @@ export const ChatHeader = ({
       <p className="font-semibold text-md text-black dark:text-white">
         {name}
       </p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator/>
+      </div>
     </div>
   )
 }
